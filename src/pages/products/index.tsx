311ce156ce7c2dbf446637e4df7038243f366b9c
jumailkv <jumijackson@gmail.com>
2025-12-09 12:02:22 +0530

    created page next-final1

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import ProductCard from '../components/productCard/ProductCard'
+import ProductCard from '../../productCard/ProductCard'
 import ProductSkeleton from '../components/skeleton/ProductSkeleton'   
 
 export default function Products() {
